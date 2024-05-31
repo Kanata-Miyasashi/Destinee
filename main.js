361d626946d50d81e08b9d7cfe54d9b684384088
Kanata <100531569+Kanata-Miyasashi@users.noreply.github.com>
2024-05-31 15:15:46 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+//var y = 0.5*x+53//
+var y = 12573/3;
+var x = (y-53)/0.5;
+
+
+
+if(y == 0.5*x+53){
+var x = (y-53)/0.5;
+console.log(x);
+alert("Le rang du terme 4191 est "+x);
+document.getElementById("hey").innerHTML = "Le rang du terme 4191 est "+x;
+}
+
